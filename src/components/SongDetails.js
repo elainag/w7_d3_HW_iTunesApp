4c98c41e1393f16fba2125e3ba0c07f07061a1ea
Elaina Gillon <elaina.gillon@gmail.com>
2022-06-29 23:28:17 +0100

    no styling added yet

--- src/components/SongDetails.js
+++ src/components/SongDetails.js
@@ -3,9 +3,15 @@ import React from "react";
 const SongDetails = ({ song, index }) => {
     const link = song.link[0].attributes.href
     return (
-        <li>
-            <a href={link} >{song.title.label} </a>
-        </li>
+
+        <div>
+            <h5># {index + 1}</h5>
+            <img src={song['im:image'][2]['label']} alt='song image' />
+            <p>{song['im:name']['label']}</p>
+            <p>{song['im:artist']['label']}</p>
+
+            <a href={link} >Listen Now </a>
+        </div>
     )
 }
 
